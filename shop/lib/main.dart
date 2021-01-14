d7d6e05522ee5313dae49ced141aeccee3df4664
Thanaphon <thanaphon.me@mail.kmutt.ac.th>
2021-01-14 23:06:32 +0700

    [INIT] shop app

---
+++ shop/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overiew_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('This is my shop'),
+      ),
+    );
+  }
+}
